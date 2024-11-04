0207308e9f4dd8fa06ed415b38dc79097787833f
Jasur <rakhmatovjasur3@gmail.com>
2024-11-04 11:48:03 +0500

    error new

---
+++ app/global-error.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react';
+
+function GlobalError() {
+    return (
+        <div>404-sahig=fa topilmadi</div>
+    );
+}
+
+export default GlobalError;
--- app/pages/404.tsx
+++
@@ -1,4 +0,0 @@
-
-export default function Custom404() {
-    return <h1>404 - Sahifa topilmadi</h1>;
-}
